d1fdcd62e5fbc6f24f658a4b0da8c41d80cc1d94
marlonsc15 <marlonsantoscorreia@gmail.com>
2022-07-27 16:21:44 -0300

    projeto finalizado

---
+++ countdown.js
@@ -0,0 +1,37 @@
+var check = false;
+
+function xequeMate() {
+    if (check == false) {
+        var timer1 = setInterval(function() { start() }, 1000);
+        var timer2 = setTimeout(function() { end() }, 13700);
+        var count = 10;
+        function start() {
+            soundBeep();
+            document.getElementById("time").innerHTML = count;
+            if (count == 0) {
+                clearInterval(timer1);
+                soundThunder();
+                document.getElementById("fire").src = "explosion.gif";
+                document.getElementById("time").innerHTML = "GAME OVER";
+            }
+            count --;
+        }
+        check = true;
+    } 
+}
+
+function soundThunder() {
+    var beep = new Audio();
+    beep.src = "assets_Thunder_Crack.mp3"
+    beep.play();
+}
+
+function soundBeep() {
+    var beep = new Audio();
+    beep.src = "assets_Beep_Short.mp3"
+    beep.play();
+}
+
+function end() {
+    document.getElementById("fire").src="clean.png";
+}
